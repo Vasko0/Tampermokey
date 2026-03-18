1fc606a2afb2e0a2a600f05fd239dc913d2c1cf8
VaskoMSI <berrkut69@gmail.com>
2026-03-18 17:40:13 +0200

    fix

--- TampermonkeyHouseCall.js
+++ TampermonkeyHouseCall.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MapsRouteHouseCall
 // @namespace    ARGO
-// @version      2024-01-20
+// @version      2026-03-18
 // @description  try to take over the world!
 // @author       Vasko
 // @match        https://pro.housecallpro.com/*
